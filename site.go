c2a5a5debb5ac1dbe142c2e524d49ee35630f95e
golfen <golfen.guo@daocloud.io>
2015-02-13 14:49:32 +0800

    no message

--- site.go
+++ site.go
@@ -2,24 +2,29 @@ package main
 
 import (
 	"fmt"
-	"strings"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 )
 
 func main() {
 	http.HandleFunc("/", hello)
 	http.HandleFunc("/env", env)
-	
-	fmt.Println("listening...")
+
+	fmt.Println("Start listening...")
+	go log()
 	err := http.ListenAndServe(":80", nil)
 	if err != nil {
 		panic(err)
 	}
+}
 
-	time.Sleep(500 * time.Millisecond)
-	fmt.Println("Hello World!")
+func log() {
+	for {
+		time.Sleep(30 * time.Second)
+		fmt.Println("Hello World!")
+	}
 }
 
 func hello(res http.ResponseWriter, req *http.Request) {
@@ -29,14 +34,11 @@ func hello(res http.ResponseWriter, req *http.Request) {
 func env(res http.ResponseWriter, req *http.Request) {
 	env := os.Environ()
 
-    fmt.Fprintln(res, "List of Environtment variables : \n")
+	fmt.Fprintln(res, "List of Environtment variables : \n")
 
-    for index, value := range env {
-       name := strings.Split(value, "=") // split by = sign
+	for index, value := range env {
+		name := strings.Split(value, "=") // split by = sign
 
-       fmt.Fprintf(res, "[%d] %s : %v\n", index, name[0], name[1])
-    }
+		fmt.Fprintf(res, "[%d] %s : %v\n", index, name[0], name[1])
+	}
 }
-    
-
-
